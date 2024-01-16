6425573c2d1a86c915da13e5bf55f183b62405c6
LDVP5049 <luismaster752@gmail.com>
2024-01-16 09:05:52 -0700

    Agregar archivos de proyecto.

---
+++ pruebaTriggers/pruebaTriggers/TriggerSample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace pruebaTriggers
+{
+    public class ChangeColor : TriggerAction<Button>
+    {
+        protected override void Invoke(Button btn)
+        {
+            btn.BackgroundColor = Color.DarkOrange;
+            btn.Text = "Holis";
+        }
+    }
+}
